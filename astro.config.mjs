4211f8631ecc80050ade0fbfea8c3380ab7b0115
lilei2603 <240924085@qq.com>
2023-12-27 10:24:27 +0800

    引入框架

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://enchanting-yeot-a641c4.netlify.app/"
+  site: "https://enchanting-yeot-a641c4.netlify.app/",
+  integrations: [preact()]
 });
